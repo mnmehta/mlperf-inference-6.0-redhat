3bab554e6a468872d7c2c12ed6b07e464ed3d7c3
Naveen Miriyalu <nmiriyal@redhat.com>
2026-02-08 23:14:00 +0000

    Correct dataset for accuracy

--- harness/scripts/check_accuracy.sh
+++ harness/scripts/check_accuracy.sh
@@ -1,5 +1,5 @@
 DIR=$1
 python3 ../language/gpt-oss-120b/eval_mlperf_accuracy.py \
         --mlperf-log ${DIR}/mlperf/mlperf_log_accuracy.json \
-        --reference-data ${DATASET_DIR}/acc/acc_eval_compliance_gpqa.parquet  \
+        --reference-data ${DATASET_DIR}/acc/acc_eval_ref.parquet  \
         --tokenizer openai/gpt-oss-120b >& ${DIR}/accuracy.txt
